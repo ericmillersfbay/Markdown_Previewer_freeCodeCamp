a4e57c79d52a1615958e507638d1084dd029e83e
Eric <Eric@Jennifer-Johns-MacBook-Pro.local>
2018-02-10 15:56:24 -0800

    initial

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  render() {
+    return (
+      <div>
+          <textarea cols="50" rows="4" type="textarea" value={this.state.input} onChange={this.handleChange}>
+          </textarea>
+        <h1 id="size">{this.state.input}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
