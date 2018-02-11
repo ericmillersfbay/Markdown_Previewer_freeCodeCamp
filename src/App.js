ed0c41aaf91342037692e823fa2529753c9eaf0d
Eric <Eric@Jennifer-Johns-MacBook-Pro.local>
2018-02-10 17:02:11 -0800

    added marked

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css';
+import marked from "react-marked"
 
 class App extends React.Component {
   constructor(props) {
@@ -15,12 +16,22 @@ class App extends React.Component {
     });
   }
   render() {
+    var marked = require('marked');
+    marked.setOptions({
+      renderer: new marked.Renderer(),
+      gfm: true,
+      tables: true,
+      breaks: false,
+      pedantic: false,
+      sanitize: false,
+      smartLists: true,
+      smartypants: false
+    });
     return (
-      
       <div>
           <textarea cols="50" rows="4" type="textarea" value={this.state.input} onChange={this.handleChange}>
           </textarea>
-        <h1 id="size">{this.state.input}</h1>
+        <h1 id="size">{marked(this.state.input)}</h1>
       </div>
     );
   }
